eab4f399093418ac220cc666de60f036f58902ea
PritiKumr <priti922474@gmail.com>
2017-09-15 19:17:12 +0530

    removed title scarping functionality. REASON - Latency goes to 12s+

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,6 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const request = require('request');
-const cheerio = require('cheerio');
 const validUrl = require('valid-url');
 
 admin.initializeApp(functions.config().firebase);
@@ -23,24 +21,17 @@ exports.sendPushNotification = functions.https.onRequest((req, res) => {
                                 return result.val()[instanceId].notificationToken;
                               });
 
-
-          request(req.query.url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              var $ = cheerio.load(body);
-              var pageTitle = $("title").text().trim() || "Link from Sling";
-              admin.messaging().sendToDevice(
-                deviceTokens, {
-                  notification: {
-                    url: req.query.url,
-                    title: pageTitle
-                  }
-                }
-              ).then(function(response) {
-                console.log("Successfully sent message:", response);
-              }).catch(function(error) {
-                console.log("Error sending message:", error);
-              });
+          admin.messaging().sendToDevice(
+            deviceTokens, {
+              notification: {
+                url: req.query.url,
+                title: "Link from Sling"
+              }
             }
+          ).then(function(response) {
+            console.log("Successfully sent message:", response);
+          }).catch(function(error) {
+            console.log("Error sending message:", error);
           });
         }
       });    
