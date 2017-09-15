f69f85ae71648d02f8de82135abf33426c9e1f61
PritiKumr <priti922474@gmail.com>
2017-09-15 19:04:41 +0530

    scraping link page title if appropriate and added to payload

--- functions/index.js
+++ functions/index.js
@@ -2,37 +2,50 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const request = require('request');
+const cheerio = require('cheerio');
+const validUrl = require('valid-url');
+
 admin.initializeApp(functions.config().firebase);
 
 
 exports.sendPushNotification = functions.https.onRequest((req, res) => {
-  const payload = {
-    notification: {
-      url: req.query.url,
-      click_action: req.query.url
-    }
-  };
 
-  admin
-    .database()
-    .ref("users/" + req.query.uid + "/readerDevices/")
-    .once("value", function(result) {
-      if(result.val()){
-        var deviceTokens = Object
-                            .keys(result.val())
-                            .map(function (instanceId) {
-                              return result.val()[instanceId].notificationToken;
-                            });
-
-        admin.messaging().sendToDevice(deviceTokens, payload).then(function(response) {
-          console.log("Successfully sent message:", response);
-        })
-        .catch(function(error) {
-          console.log("Error sending message:", error);
-        });
+  if (validUrl.isUri(req.query.url)){
+    admin
+      .database()
+      .ref("users/" + req.query.uid + "/readerDevices/")
+      .once("value", function(result) {
+        if(result.val()){
+          var deviceTokens = Object
+                              .keys(result.val())
+                              .map(function (instanceId) {
+                                return result.val()[instanceId].notificationToken;
+                              });
 
 
-      }
-    });
+          request(req.query.url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var $ = cheerio.load(body);
+              var pageTitle = $("title").text().trim() || "Link from Sling";
+              admin.messaging().sendToDevice(
+                deviceTokens, {
+                  notification: {
+                    url: req.query.url,
+                    title: pageTitle
+                  }
+                }
+              ).then(function(response) {
+                console.log("Successfully sent message:", response);
+              }).catch(function(error) {
+                console.log("Error sending message:", error);
+              });
+            }
+          });
+        }
+      });    
+  } else {
+    console.log('URL invalid', req.query.url)
+  }
   res.status(200).end();
 });
